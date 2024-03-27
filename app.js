8c2666d9f57695f599063559f90d778317ea17ec
mayk0l <maykol0219@gmail.com>
2024-03-27 01:58:03 -0300

    implemented more djs sets and links to and links backs

--- app.js
+++ app.js
@@ -2,14 +2,45 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Ruta a la carpeta de archivos estáticos
 app.use('/audio-files', express.static(path.join(__dirname, 'audio-files')));
 
-// Endpoint para renderizar la página HTML con el reproductor de audio
+// Lista de DJ sets disponibles
+const djSets = [
+  { name: 'si-no-duermes-sale-mejor', title: 'Si No Duermes, Sale Mejor' },
+  { name: 'de-la-hardgroove-hipnotica-b2b-cinder', title: 'De la HardGroove Hipnotica b2b Cinder' }
+];
+
+// Endpoint para renderizar la página HTML con enlaces a cada DJ set
 app.get('/audio', (req, res) => {
-  const audioFileName = 'si-no-duermes-sale-mejor.mp3'; // Nombre del archivo dentro de la carpeta 'audio-files'
+  // Construir la lista de enlaces a cada DJ set
+  const djSetLinks = djSets.map(djSet => `<li><a href="/audio/${djSet.name}">${djSet.title}</a></li>`).join('');
+
+  // Renderizar la página HTML con los enlaces a cada DJ set
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>DJ Sets Disponibles</title>
+    </head>
+    <body>
+      <h1>DJ Sets Disponibles</h1>
+      <ul>
+        ${djSetLinks}
+      </ul>
+    </body>
+    </html>
+  `);
+});
+
+// Endpoint para renderizar la página HTML con el reproductor de audio
+app.get('/audio/:djSet', (req, res) => {
+  const djSet = req.params.djSet; // Obtener el nombre del DJ set desde los parámetros de la URL
+  const audioFileName = `${djSet}.mp3`; // Construir el nombre del archivo de audio
   const audioUrl = `/audio-files/${audioFileName}`; // URL relativa al archivo dentro de la carpeta 'audio-files'
 
   // Renderizar la página HTML con el reproductor de audio
@@ -19,14 +50,15 @@ app.get('/audio', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Reproductor de audio</title>
+      <title>Reproductor de audio para ${djSet}</title>
     </head>
     <body>
-      <h1>Reproductor de audio</h1>
+      <h1>Reproductor de audio para ${djSet}</h1>
       <audio controls autoplay>
         <source src="${audioUrl}" type="audio/mpeg">
         Tu navegador no soporta la reproducción de audio.
       </audio>
+      <p><a href="/audio">Volver a DJ Sets Disponibles</a></p>
     </body>
     </html>
   `);
